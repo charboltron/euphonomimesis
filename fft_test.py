71ae23b92acfe91ee6980344a1195312dbc4e4a0
charboltron <boltch@pdx.edu>
2020-02-28 20:48:29 -0800

    added display plotter, cleaned up fft, modclips dir

--- fft_test.py
+++ fft_test.py
@@ -1,4 +1,5 @@
 from numpy.fft import fft, fftfreq, ifft
+import scipy.fftpack
 from playsound import playsound as ps
 import struct
 import numpy as np
@@ -19,87 +20,55 @@ sampwidth=2
 cwd = os.getcwd()
 
 clip_dir = cwd+'/clips/'
-
-# Get the signal file.
 waves = [file for file in os.listdir(clip_dir) if '.wav' in file]
-# waves = [file for file in os.listdir() if '.wav' in file]
 
 for wave in range(len(waves)):
     wv = clip_dir+waves[wave]
-    wavfile = wav.open(wv, 'rb')
-    #play the sound
-    # ps(wv)
-    # Channels per frame.
-    channels = wavfile.getnchannels()
-    # Bytes per sample.
-    width = wavfile.getsampwidth()
-    # Sample rate
-    rate = wavfile.getframerate()
-    # Number of frames.
-    frames = wavfile.getnframes()
-    # Length of a frame
+    current_clip = wav.open(wv, 'rb')
+    ps(wv)
+    channels = current_clip.getnchannels()
+    width = current_clip.getsampwidth()
+    rate = current_clip.getframerate()
+    frames = current_clip.getnframes()
     frame_width = width * channels
 
-    # Get the signal and check it.
     max_sample = None
     min_sample = None
-    wave_bytes = wavfile.readframes(frames)
-    # Iterate over frames.
+    wave_bytes = current_clip.readframes(frames)
     samples = []
     for f in range(0, len(wave_bytes), frame_width):
         frame = wave_bytes[f : f + frame_width]
-        #print(frame)
-        # Iterate over channels.
+
         for c in range(0, len(frame), width):
-            # Build a sample.
             sample_bytes = frame[c : c + width]
-            # XXX Eight-bit samples are unsigned
             sample = int.from_bytes(sample_bytes,
                                     byteorder='little',
                                     signed=(width>1))
-            #print(sample)
             sample = float(sample)
             samples.append(sample)
-            # Check extrema.
-            #if max_sample == None:
-            #    max_sample = sample
-            #if min_sample == None:
-            #    min_sample = sample
-            #if sample > max_sample:
-            #    max_sample = sample
-            #if sample < min_sample:
-            #    min_sample = sample
 
-    import scipy.fftpack
+    print(f'max sample = {max_sample}, min sample = {min_sample}')
 
-    # Number of samplepoints
-    # sample spacing
+
     T = 1.0 / num_samples
-    # x = np.linspace(0.0, num_samples*T, num_samples)
     yf = scipy.fftpack.rfft(samples)
     xf = np.linspace(0.0, 1.0/(2.0*T), num_samples/2)
 
     iyf = scipy.fftpack.irfft(yf, len(samples))
-    for i in range(0, 20000, 1000):
-        print(samples[i], iyf[i])
+
     f = 'new_sound_file.wav'
+
     wav_file = wav.open(f, 'w')
-    wav_file.setparams((nchannels, sampwidth, int(sampling_rate), nframes, comptype, compname))
+    wav_file.setparams(current_clip.getparams())
     print(len(samples), len(iyf))
-    samples -= amplitude//2
-    for samp in samples:
-        print(samp*amplitude)   
-        wav_file.writeframes(struct.pack('h', int(samp*amplitude)//2))
-    ps(cwd+f)
+    for samp in iyf:
+        wav_file.writeframes(struct.pack('h', int(samp)))
+    ps(cwd+'/'+f)
     exit()
 
-    # print(yf)
-    # print(f'min yf = {min(yf)}, max yf = {max(yf)}')
-    # print(f'min xf = {min(xf)}, max xf = {max(xf)}')
-    # # exit()
     # fig, ax = plt.subplots()
     # ax.plot(xf, 2.0/num_samples * np.abs(yf[:num_samples//2]))
     # print(2.0/num_samples * np.abs(yf[:num_samples//2]))
     # plt.show()
 
-    wavfile.close()
+    current_clip.close()
